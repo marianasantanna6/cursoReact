e31087f1dc650936f5f2eebb0b900fb52fd06a96
Mariana <marianapaulino2601@gmail.com>
2026-02-02 17:54:03 -0300

    commit inicial

---
+++ src/components/AddTask.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import Input from "./Input";
+
+function AddTask({ onAddTaskSubmit }) {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+
+
+  return (
+    <div className="space-y-4 p-6 bg-slate-200 rounded-md shadow flex flex-col">
+
+      <Input
+        type="text"
+        placeholder="Título da tarefa"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+
+      <Input
+        type="text"
+        placeholder="Digite a descrição da tarefa"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+    
+
+      <button
+        onClick={() => {
+            //verificar se o título e a descrição não estão vazios
+            if (!title.trim() || !description.trim()) 
+            return alert("Por favor, preencha o título e a descrição da tarefa.");
+
+            onAddTaskSubmit(title, description);
+            setTitle("");
+            setDescription("");
+        }}
+        className="bg-slate-500 text-white px-4 py-2 rounded-md font-medium"
+      >
+        Adicionar
+      </button>
+
+    </div>
+  );
+}
+
+export default AddTask;
---
+++ src/components/Input.jsx
@@ -0,0 +1,10 @@
+function Input(props) {
+return (
+    <input
+        className="w-full rounded-md border border-slate-300 outline-slate-400 px-4 py-2"
+        {...props}
+      />
+)
+}
+
+export default Input;
